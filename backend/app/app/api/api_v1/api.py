d8b8dfd55a5a6300e11aceb1e74367f1816bf27d
Dmitriy <trueprogramdevelop@gmail.com>
2021-05-13 16:12:35 +0300

    add statistic route

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -14,10 +14,11 @@ __version__ = '20210503'
 
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils
+from app.api.api_v1.endpoints import items, login, users, utils, statistic
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(items.router, prefix="/statistic", tags=["statistic"])
